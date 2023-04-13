0fe94a26b45e494069b3371d5e3dff7bd4b66994
Kailen James <sevenflags57@gmail.com>
2023-04-12 20:41:17 -0700

    Add type hints

--- intermodal-logistics/scripts/globals.lua
+++ intermodal-logistics/scripts/globals.lua
@@ -3,22 +3,24 @@
 -- These are for the ingame global object
 function init_static_globals()
 	if (global.gantry_clusters == nil) then
-		-- Array containing every gantry cluster
+		---@type GantryCluster[] Array containing every gantry cluster
 		global.gantry_clusters = {};
 	end
 
 	if (global.gantry_controllers == nil) then
-		-- Map containing every gantry entity mapped to its lua data
+		---@type {[number]: GantryController} Map containing every gantry entity mapped to its lua data
 		global.gantry_controllers = {};
 	end
 
 	if (global.sockets == nil) then
-		-- Map containing every socket entity mapped to its lua data
+		---@type {[number]: ContainerSocket} Map containing every socket entity mapped to its lua data
 		global.sockets = {};
 	end
 end
 
 -- These are more generic non serialized globals
 function init_transient_globals()
-	--TODO:
+	-- There are none so far.
+	-- Maybe I'll need some later.
+	-- That's what this function is for.
 end
